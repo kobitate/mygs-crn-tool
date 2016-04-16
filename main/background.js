da294c857992f13c4bdee8b69140ba1beac116d5
Kobi Tate <kobi@kobitate.com>
2016-04-15 23:31:20 -0400

    Fixed a bug where the welcome page would launch inappropriately

--- main/background.js
+++ main/background.js
@@ -1,4 +1,6 @@
 /* global chrome */
-chrome.runtime.onInstalled.addListener(function () {
-    chrome.tabs.create({url: chrome.extension.getURL("install/index.html")});
+chrome.runtime.onInstalled.addListener(function (details) {
+    if (details.reason === "install") {
+        chrome.tabs.create({url: chrome.extension.getURL("install/index.html")});
+    }
 });
